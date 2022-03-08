51d1dec1a5ac03426c7deef5a4b4509cb3567b19
bunnie <bunnie@kosagi.com>
2022-03-08 19:18:49 +0800

    add some documentation

--- src/api.rs
+++ src/api.rs
@@ -1,6 +1,7 @@
 pub const SERVER_NAME_TTS_EXEC: &str     = "_Text To Speech Executable_ / (external C program)";
 pub const MAX_WAV_BUF_SAMPLES: usize = 1024;
 
+/// Back end opcode table
 #[derive(num_derive::FromPrimitive, num_derive::ToPrimitive, Debug)]
 pub enum TtsBeOpcode {
     /// Take a string and translate it to a WAV file
@@ -11,17 +12,21 @@ pub enum TtsBeOpcode {
     Quit,
 }
 
+/// Backend-originated control messages that can be sent to the front end
 #[derive(Debug, Copy, Clone, rkyv::Archive, rkyv::Serialize, rkyv::Deserialize)]
 pub enum TtsBeControl {
     Abort,
     End,
 }
 
+/// Messages to the backend. Currently, just the text string can be sent, and it is
+/// limited to 2048 characters in length.
 #[derive(Debug, Copy, Clone, rkyv::Archive, rkyv::Serialize, rkyv::Deserialize)]
 pub struct TtsBackendMsg {
     pub text: xous_ipc::String::<2048>,
 }
 
+/// Configuration data for the backend
 #[derive(Debug, Copy, Clone, rkyv::Archive, rkyv::Serialize, rkyv::Deserialize)]
 pub struct TtsBackendConfig {
     pub sid: [u32; 4],
@@ -29,6 +34,8 @@ pub struct TtsBackendConfig {
     pub samples_per_cb: Option<u32>,
 }
 
+/// Data returned by the backend to the `dedicated_sid`, routed to the opcode
+/// as specified in the callback specifier.
 #[derive(Debug, Copy, Clone, rkyv::Archive, rkyv::Serialize, rkyv::Deserialize)]
 pub struct TtsBackendData {
     /// the 16-bit wave sample data being passed back.
--- src/lib.rs
+++ src/lib.rs
@@ -6,11 +6,14 @@ use xous::CID;
 use xous_ipc::Buffer;
 use num_traits::ToPrimitive;
 
+/// API for Text-to-speech backends on Xous. Third party executables for TTS
+/// that comply to this API can be used by Xous for doing text to speech operations.
 #[derive(Debug)]
 pub struct TtsBackend {
     conn: CID,
 }
 impl TtsBackend {
+    /// Creates the TTS interface object
     pub fn new(xns: &xous_names::XousNames) -> Result<Self, xous::Error> {
         REFCOUNT.fetch_add(1, Ordering::Relaxed);
         let conn = xns.request_connection_blocking(api::SERVER_NAME_TTS_EXEC).expect("Can't connect to TtsBackend server");
@@ -18,7 +21,19 @@ impl TtsBackend {
             conn,
         })
     }
-
+    /// Configure the text to speech backend. A dedicated server identified via `dedicated_sid`
+    /// server is recommend for handling the callbacks from the backend, because the backend
+    /// is not at all trusted and once the backend has your SID it can invoke any methods made
+    /// available through a server's API.
+    ///
+    /// The `opcode` field is a unique number assigned to the message sent to the dedicated SID that
+    /// can help with message routing. Usually there is at least a `Quit` opcode available on every
+    /// server, hence the need for the routing ID.
+    ///
+    /// `samples_per_cb` is an optional field that can specify how many samples per callback is desired.
+    /// This field can be entirely ignored, and is just a configuration hint to the backend.
+    ///
+    /// The server assumes that wave data will come in at 8kHz sampling rate, 16-bit unsigned mono.
     pub fn tts_config(&self, dedicated_sid: [u32; 4], opcode: u32, samples_per_cb: Option<u32>) -> Result<(), xous::Error> {
         let msg = TtsBackendConfig {
             sid: dedicated_sid,
@@ -28,7 +43,10 @@ impl TtsBackend {
         let buf = Buffer::into_buf(msg).or(Err(xous::Error::InternalError))?;
         buf.lend(self.conn, TtsBeOpcode::RegisterCb.to_u32().unwrap()).map(|_| ())
     }
-
+    /// Translate the specified `text` into a wave file. The server should abort any currently on-going
+    /// translation if this is interrupting an ongoing operation.
+    ///
+    /// The string is a stanard Rust `utf-8` format string, and currently the assumed langauge is English.
     pub fn tts_simple(&self, text: &str) -> Result<(), xous::Error> {
         let msg = TtsBackendMsg {
             text: xous_ipc::String::from_str(text),
